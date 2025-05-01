50305bd17d1b093e60640bfd93bf027a67771e33
Steve Khuu <steve.khuu@conductive.ai>
2025-05-01 10:54:51 -0400

    moving to nextjs infrastructure

---
+++ next.config.mjs
@@ -0,0 +1,19 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  images: {
+    unoptimized: true,
+  },
+  output: "export",
+  basePath: "/stevekhuu.github.io",
+  images: {
+    unoptimized: true,
+  },
+}
+
+export default nextConfig
